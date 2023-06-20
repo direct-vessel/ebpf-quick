68fd4b64d88c81538dcd328972c53e62302ebd38
fanfanbj <fanfanbj@outlook.com>
2023-06-20 09:56:26 +0100

    Update bpf controller, modify delete cycle

--- ebpf-operator/internal/controller/bpf_controller.go
+++ ebpf-operator/internal/controller/bpf_controller.go
@@ -25,12 +25,14 @@ import (
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/utils/pointer"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	bpfv1 "ebpf-operator/api/v1"
 )
@@ -61,11 +63,16 @@ func (r *BPFReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	ebpf := &bpfv1.BPF{}
 	log.Info("fetching BPF Resource")
 	if err := r.Get(ctx, req.NamespacedName, ebpf); err != nil {
-		log.Error(err, "unable to fetch ebpf")
 		// we'll ignore not-found errors, since they can't be fixed by an immediate
 		// requeue (we'll need to wait for a new notification), and we can get them
 		// on deleted requests.
-		return ctrl.Result{}, client.IgnoreNotFound(err)
+		if errors.IsNotFound(err) {
+			log.Info(" ebpf not found")
+			return reconcile.Result{}, nil
+		}
+
+		log.Error(err, "unable to fetch ebpf")
+		return ctrl.Result{}, err
 	}
 
 	// name of our custom finalizer
